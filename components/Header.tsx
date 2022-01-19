aabef7dd8dd476b328aa36657e763f7d5db2b927
Paul Gonzalez <robertgonzalez018@gmail.com>
2022-01-19 12:07:54 -0800

    added sanityyt to app.

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+function Header() {
+  return (
+    <header className="">
+      <div className="">
+        <Link href="/">
+          <img src="https://links.papareact.com/yvf" alt="" />
+        </Link>
+      </div>
+      <div className=""></div>
+    </header>
+  );
+}
+
+export default Header;
