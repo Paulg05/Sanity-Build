a7a3277b56e9388c7f9678507fde992bda047aca
Paul Gonzalez <robertgonzalez018@gmail.com>
2022-01-20 10:02:55 -0800

    added description to the post schema file in sanity file, and defined what a post looks like in TS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import { sanityClient, urlFor } from "../sanity";
 
-export default function Home() {
+interface Props {
+  posts: [Post]
+}
+
+export default function Home(props: Props) {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -9,8 +14,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <div className="flex justify-between items-center bg-yellow-300
-      border-y border-black py-10 lg:py-0">
+      <div
+        className="flex justify-between items-center bg-yellow-300
+      border-y border-black py-10 lg:py-0"
+      >
         <div className="px-10 space-y-5">
           <h1 className="text-6xl max-w-xl font-serif">
             <span className="underline decoration-black decoration-4">
@@ -35,9 +42,27 @@ export default function Home() {
   );
 }
 
-//to make SSR work within next.js we use specual func() called 
+//to make SSR work within next.js we use specual func() called
 
-export const getServerSideProps = async() => {
-  //query to fetch info from sanity 
-  const query = 
-}
+export const getServerSideProps = async () => {
+  //query to fetch info from sanity
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author -> {
+    name, 
+    image
+  },
+  description, 
+  mainImage, 
+  slug
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
---
+++ typing.d.ts
@@ -0,0 +1,5 @@
+//definition TS file in this case defines what a post looks like 
+export interface Post {
+  _id: string, 
+  _createdAt: string
+}
