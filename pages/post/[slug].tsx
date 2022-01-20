fd927b75a64c1575ac13997a410f16cc5b199b42
Paul Gonzalez <robertgonzalez018@gmail.com>
2022-01-20 15:26:05 -0800

    implemented ISR and revalidation for caching old pages with new informatino after 60sec

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { GetStaticProps } from "next";
 import Header from "../../components/Header";
 import { sanityClient, urlFor } from "../../sanity";
+import { Post } from "../../typing";
 
-function Post() {
+interface Props {
+  post: Post;
+}
+
+function Post({ post }: Props) {
+  console.log(post);
+  
   return (
     <main>
       <Header />
@@ -12,7 +20,8 @@ function Post() {
 
 export default Post;
 
-// func() that tells next.js with paths to pre-build
+// func() that returns an array of paths which gives us all the slugs that
+// were going to need back
 export const getStaticPaths = async () => {
   const query = `*[_type == "post"]{
     _id,
@@ -23,4 +32,49 @@ export const getStaticPaths = async () => {
   const posts = await sanityClient.fetch(query);
 
   // todo: figure out the paths
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }));
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+//uses all the slugs to fetch information for each page
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type == "post" && slug.current == $slug][0]{
+    _id, 
+    title, 
+    author -> {
+    name, 
+    image
+  },
+   'comments': *[
+    _type == "comment" &&
+    post._ref == ^._id &&
+    approved == true],
+    description, 
+    mainImage,
+    slug, 
+  body
+  }`;
+
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  });
+  if (!post) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: {
+      post,
+    },
+    //updates the cache that revalidates the ISR
+    revalidate: 60, //after 60 sec, this updates the old cached version 
+  };
 };
