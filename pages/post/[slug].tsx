05e2b571e1ad30de58b0807cf46156db22878379
Paul Gonzalez <robertgonzalez018@gmail.com>
2022-01-21 10:52:25 -0800

    added form feature for the different blog paths for name, email, and comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -36,15 +36,62 @@ function Post({ post }: Props) {
             Published at {new Date(post._createdAt).toLocaleString()}
           </p>
         </div>
-        <div className="">
+        <div className="mt-10">
           <PortableText
+            className=""
             dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
             projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
             content={post.body}
-            serializers={}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="text-2xl font-bold my-5" {...props} />
+              ),
+              h2: (props: any) => (
+                <h1 className="text-xl font-bold my-5" {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className="ml-4 list-disc">{children} </li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
           />
         </div>
       </article>
+      <hr className="max-w-lg border-yellow-500 border mx-auto my-5" />
+
+      <form className="flex flex-col p-5 my-10 max-w-2xl mx-auto mb-10">
+        <h3 className="text-sm text-yellow-500">Enjoy the article</h3>
+        <h4 className="text-3xl font-bold">Leave a comment below</h4>
+        <hr className="py-3 mt-2" />
+
+        <label className="block mb-5">
+          <span className="text-gray-700">Name</span>
+          <input
+            className="shadow border rounded py-2 px-3 form-input mt-1 block
+          w-full ring-yellow-500 outline-none focus:ring"
+            placeholder="Username"
+            type="text"
+          />
+        </label>
+        <label className="block mb-5">
+          <span className="text-gray-700">Email</span>
+          <input
+            className="shadow border rounded py-2 px-3 form-input mt-1 block
+          w-full ring-yellow-500 outline-none focus:ring"
+            placeholder="Username"
+            type="text"
+          />
+        </label>
+        <label className="block mb-5">
+          <span className="text-gray-700">Comment</span>
+          <textarea className="shadow border rounded py-2 px-3 form-textarea mt-1
+          block w-full ring-yellow-500 outline-none focus:ring" placeholder="Username" rows={8} />
+        </label>
+      </form>
     </main>
   );
 }
