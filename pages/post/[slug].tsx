178b2a0a43c7fab5b69ae47ee2b27d89943d072a
Paul Gonzalez <robertgonzalez018@gmail.com>
2022-01-21 11:53:37 -0800

    installed react-hook-forms to require name, email, comments and pass data to the backend

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -4,12 +4,30 @@ import Header from "../../components/Header";
 import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../typing";
 import PortableText from "react-portable-text";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface IFormInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment: string;
+}
 
 interface Props {
   post: Post;
 }
 
 function Post({ post }: Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+    console.log(data);
+  };
+
   return (
     <main>
       <Header />
@@ -63,14 +81,19 @@ function Post({ post }: Props) {
       </article>
       <hr className="max-w-lg border-yellow-500 border mx-auto my-5" />
 
-      <form className="flex flex-col p-5 my-10 max-w-2xl mx-auto mb-10">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col p-5 my-10 max-w-2xl mx-auto mb-10"
+      >
         <h3 className="text-sm text-yellow-500">Enjoy the article</h3>
         <h4 className="text-3xl font-bold">Leave a comment below</h4>
         <hr className="py-3 mt-2" />
 
+        <input {...register("_id")} type="hidden" name="_id" value={post._id} />
         <label className="block mb-5">
           <span className="text-gray-700">Name</span>
           <input
+            {...register("name", { required: true })}
             className="shadow border rounded py-2 px-3 form-input mt-1 block
           w-full ring-yellow-500 outline-none focus:ring"
             placeholder="Username"
@@ -80,17 +103,43 @@ function Post({ post }: Props) {
         <label className="block mb-5">
           <span className="text-gray-700">Email</span>
           <input
+            {...register("email", { required: true })}
             className="shadow border rounded py-2 px-3 form-input mt-1 block
           w-full ring-yellow-500 outline-none focus:ring"
             placeholder="Username"
-            type="text"
+            type="email"
           />
         </label>
         <label className="block mb-5">
           <span className="text-gray-700">Comment</span>
-          <textarea className="shadow border rounded py-2 px-3 form-textarea mt-1
-          block w-full ring-yellow-500 outline-none focus:ring" placeholder="Username" rows={8} />
+          <textarea
+            {...register("comment", { required: true })}
+            className="shadow border rounded py-2 px-3 form-textarea mt-1
+          block w-full ring-yellow-500 outline-none focus:ring"
+            placeholder="Username"
+            rows={8}
+          />
         </label>
+        {/*errors that are returned when the field validation is failed */}
+        <div className="flex flex-col p-5">
+          {errors.name && (
+            <span className="text-red-500">- The Name Field is required</span>
+          )}
+          {errors.email && (
+            <span className="text-red-500">- The Email Field is required</span>
+          )}
+          {errors.comment && (
+            <span className="text-red-500">
+              - The Comment Field is required
+            </span>
+          )}
+        </div>
+        <input
+          type="submit"
+          className="shadow bg-yellow-500 hover:bg-yellow-400
+        focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4
+        rounded cursor-pointer"
+        />
       </form>
     </main>
   );
